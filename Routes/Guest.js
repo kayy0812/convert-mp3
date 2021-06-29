5a3eaac5fb2421c504df5c70fda7d6112c6521a7
Van Loc <levanloc8112@gmail.com>
2021-06-29 21:31:49 +0700

    Sieucap updated

--- Routes/Guest.js
+++ Routes/Guest.js
@@ -1,31 +1,44 @@
+const { assert } = require('console');
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-
+const contentDisposition = require('content-disposition');
+const information = {
+	title: 'KhongCoTieuDe'
+}
 module.exports = (function() {
 
-    router.get('/', function(req, res) {
-    	res.send('index');
+    router.get('/', function(req, res, next) {
+    	res.send('Trang web đang cập nhật');
     });
 
-    router.get('/convert_mp3/:url', function(req, res) { 	
-    	ytdl.getInfo(req.params.url).then(info => {
-    		const _mp3 = ytdl(req.params.url, {quality: 'highestaudio'});
-    		const path_mp3 = './Temp/mp3/' + info.videoDetails.videoId + '.mp3';
-    		if (!fs.existsSync(path_mp3)) {
-    			_mp3.pipe(fs.createWriteStream(path_mp3));
-    		    // _mp3.on('progress', (chunkLength, downloaded, total) => {
-    		    // 	const percent = ((downloaded / total) * 100).toFixed(2);
-    		    // 	console.log(percent);
-    		    // });
-    		    _mp3.on('end', () => {
-    		    	res.download(path_mp3, info.videoDetails.title + '.wav');
-    		    });
-    		} else {	    
-    		    res.download(path_mp3, info.videoDetails.title + '.wav');
-    		}
-    	});
-    });	
+    router.get('/youtube/mp3/:url', async(req, res) => {
+		if (!req.params.url || req.params.url.length < 1) return false;
+		const {
+			player_response: {
+			  videoDetails: { title, author },
+			},
+		} = await ytdl.getBasicInfo(req.params.url);
+		
+		const _mp3 = ytdl(req.params.url, {
+			quality: 'highestaudio'
+		});
+		res.setHeader(
+			'Content-Disposition',
+			contentDisposition(title + '.mp3'),
+			'Content-Type',
+			'audio/mp3',
+			'Content-Transfer-Encoding',
+			'binary',
+			'Cache-Control',
+			'must-revalidate, post-check=0, pre-check=0'
+		);
+		_mp3.pipe(res);
+		_mp3.on('progress', (chunkLength, downloaded, total) => {
+			const percent = ((downloaded / total) * 100).toFixed(2);
+			console.log(percent);
+		});
+    });
     return router;
 })();
